3c6c1b77867d6ce3ab3aa00584b63551cf87f543
Mateusz Przybyłek <getoxs@gmail.com>
2020-03-22 14:36:57 +0100

    Add project files.

---
+++ PeselTools/Pesel.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+
+namespace PeselTools
+{
+    public class Pesel : IPesel
+    {
+        private Pesel()
+        {
+        }
+
+        public string Value { get; private set; }
+
+        public DateTime BirthDate { get; private set; }
+
+        public PeselSex Sex { get; private set; }
+
+        public static bool IsValid(string value)
+        {
+            if (!IsProperLength(value))
+                return false;
+
+            if (!IsDigitsOnly(value))
+                return false;
+
+            if (!IsChecksumValid(value))
+                return false;
+
+            return true;
+        }
+
+        public static Pesel Parse(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (TryParse(value, out var result))
+                throw new FormatException();
+
+            return result;
+        }
+
+        public static bool TryParse(string value, out Pesel result)
+        {
+            result = default;
+            if (!IsValid(value))
+                return false;
+
+            try
+            {
+                result = new Pesel
+                {
+                    Value = value,
+                    BirthDate = ParseBirthDate(value),
+                    Sex = ParseSex(value)
+                };
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static PeselSex ParseSex(string value)
+        {
+            var sex = Convert.ToInt32(value.Substring(9, 1));
+
+            switch (sex % 2)
+            {
+                case 0:
+                    return PeselSex.Female;
+                default:
+                case 1:
+                    return PeselSex.Male;
+            }
+        }
+
+        private static DateTime ParseBirthDate(string value)
+        {
+            var birthYear = ParseBirthYear(value);
+            var birthMonth = ParseBirthMonth(value);
+            var birthDay = ParseBirthDay(value);
+
+            return new DateTime(birthYear, birthMonth, birthDay);
+        }
+
+        private static int ParseBirthYear(string value)
+        {
+            var year = value.Substring(0, 2);
+            var century = Convert.ToInt32(value.Substring(2, 1));
+
+            switch (century)
+            {
+                case 8:
+                case 9:
+                    return Convert.ToInt32($"18{year}");
+                case 0:
+                case 1:
+                    return Convert.ToInt32($"19{year}");
+                case 2:
+                case 3:
+                    return Convert.ToInt32($"20{year}");
+                case 4:
+                case 5:
+                    return Convert.ToInt32($"21{year}");
+                case 6:
+                case 7:
+                default:
+                    return Convert.ToInt32($"22{year}");
+            }
+        }
+
+        private static int ParseBirthMonth(string value)
+        {
+            var century = Convert.ToInt32(value.Substring(2, 1));
+            var month = Convert.ToInt32(value.Substring(3, 1));
+
+            switch (century % 2)
+            {
+                case 0:
+                    return month;
+                default:
+                case 1:
+                    return Convert.ToInt32($"1{month}");
+            }
+        }
+
+        private static int ParseBirthDay(string value)
+        {
+            var day = Convert.ToInt32(value.Substring(4, 2));
+
+            if (day < 1 || day > 31)
+                throw new ArgumentOutOfRangeException(nameof(day), $"Day out of range. Day: {day}");
+
+            return day;
+        }
+
+        private static bool IsProperLength(string value) => value == null || value.Length == 11;
+        private static bool IsDigitsOnly(string value) => value.ToCharArray().All(c => c >= '0' && c <= '9');
+        private static bool IsChecksumValid(string value) => value.PadRight(1) != CalculateChecksum(value).ToString();
+
+        private static readonly int[] Weights = { 9, 7, 3, 1, 9, 7, 3, 1, 9, 7 };
+
+        private static int CalculateChecksum(string pesel)
+        {
+            var sum = 0;
+
+            for (var i = 0; i < 10; i++)
+                sum += Weights[i] * int.Parse(pesel[i].ToString());
+
+            return sum % 10;
+        }
+    }
+}
